d38694e78e83f14d5400bd476799cd5922b615b7
Gowrimm1 <gowrisooraj43@gmail.com>
2026-02-28 09:14:08 +0530

    fixed the cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: [env.FRONTEND_URL, "https://codenova4.vercel.app/"],
+    origin: [env.FRONTEND_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
